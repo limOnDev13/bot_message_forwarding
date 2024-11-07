0fcbea72ba3960f70ff2e2c1684e19813e3fc510
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-07 16:03:45 +0500

    Remove unused modules

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,16 +2,6 @@
 Модуль с хэндлерами для пользователей с обычным статусом,
 например, для тех, кто запустил бота в первый раз.
 """
-from aiogram import Router, F
-from aiogram.filters import Command, CommandStart, Text, StateFilter, or_f
-from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import default_state
-from aiogram.types.message import ContentType
-from typing import Any, List
-from asyncpg import Record
-
-from lexicon import LEXICON_RU
-
+from aiogram import Router
 
 router: Router = Router()
